985272e8d9f8afed0c8c7f52fb800ef2bb65e1d8
Pablo CR <pablocarderam@gmail.com>
2014-01-08 20:30:05 -0500

    Graph mod
    Minor edit, makes lines on top and bottom of graph appear clearer

--- js/graphs.js
+++ js/graphs.js
@@ -11,16 +11,16 @@ function graph() { // draw population and resource graphs
 	ctx.strokeStyle = '#FF0000'; // draw population curve
 	ctx.beginPath();
 	for (var i=1; i<graphInfo.length; i++) {
-		ctx.moveTo((i-1)*centerX*2/graphInfo.length, (graphInfo[i-1][0]*(centerY*2)/maxBugs)*(-1) + centerY*2); // move line from last point...
-		ctx.lineTo(i*centerX*2/graphInfo.length, (graphInfo[i][0]*(centerY*2)/maxBugs)*(-1) + centerY*2); // ...to next
+		ctx.moveTo((i-1)*centerX*2/graphInfo.length, (graphInfo[i-1][0]*(centerY*2 - ctx.lineWidth)/maxBugs)*(-1) + centerY*2); // move line from last point...
+		ctx.lineTo(i*centerX*2/graphInfo.length, (graphInfo[i][0]*(centerY*2 - ctx.lineWidth)/maxBugs)*(-1) + centerY*2); // ...to next
 	}
 	ctx.stroke();
 	
 	ctx.strokeStyle = '#339900'; // draw resources curve
 	ctx.beginPath();
 	for (var j=1; j<graphInfo.length; j++) {
-		ctx.moveTo((j-1)*centerX*2/graphInfo.length, (graphInfo[j-1][1]*(centerY*2)/maxResources)*(-1) + centerY*2);
-		ctx.lineTo(j*centerX*2/graphInfo.length, (graphInfo[j][1]*(centerY*2)/maxResources)*(-1) + centerY*2);
+		ctx.moveTo((j-1)*centerX*2/graphInfo.length, (graphInfo[j-1][1]*(centerY*2 - ctx.lineWidth)/maxResources)*(-1) + centerY*2);
+		ctx.lineTo(j*centerX*2/graphInfo.length, (graphInfo[j][1]*(centerY*2 - ctx.lineWidth)/maxResources)*(-1) + centerY*2);
 	}
 	ctx.stroke();
 	
